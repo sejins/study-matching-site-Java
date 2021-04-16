9433a975a9187fd092e4131442dd8e5ca7239393
sejin <pengang1011@naver.com>
2021-04-17 01:14:53 +0900

    postgreSQL 설정

--- src/main/java/com/sejin/mail/ConsoleMailSender.java
+++ src/main/java/com/sejin/mail/ConsoleMailSender.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import javax.mail.internet.MimeMessage;
 import java.io.InputStream;
 
-@Profile("local") // profile 이 "local"일 때 빈으로 등록이 된다.
+@Profile({"local","dev"}) // profile 이 "local" 또는 "dev"일 때 빈으로 등록이 된다.
 @Component
 @Slf4j
 public class ConsoleMailSender implements JavaMailSender {
@@ -28,7 +28,6 @@ public class ConsoleMailSender implements JavaMailSender {
 
     @Override
     public void send(MimeMessage mimeMessage) throws MailException {
-
     }
 
     @Override
