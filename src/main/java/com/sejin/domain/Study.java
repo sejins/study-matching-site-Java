ab612de2f6600d69c690073797d1606b4bb8ee80
sejin <pengang1011@naver.com>
2021-04-24 22:20:18 +0900

    Study 도메인 생성

---
+++ src/main/java/com/sejin/domain/Study.java
@@ -0,0 +1,56 @@
+package com.sejin.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Study {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToMany
+    private Set<Account> managers;
+
+    @ManyToMany
+    private Set<Account> members;
+
+    @ManyToMany
+    private Set<Zone> zones;
+
+    @ManyToMany
+    private Set<Tag> tags;
+
+    @Column(unique = true)
+    private String path;
+
+    private String title;
+
+    private String shortDescription;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String fullDiscription;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String image;
+
+    private LocalDateTime publishedDateTime;
+
+    private LocalDateTime closedDateTime;
+
+    private LocalDateTime recruitingUpdateDateTime;
+
+    private boolean recruiting;
+
+    private boolean published;
+
+    private boolean closed;
+
+    private boolean useBanner;
+}
